5a0f7770ff1969872a327c3d56eb3a3ba0538773
wurh <ro87630872@126.com>
2019-12-22 10:05:18 +0800

    【UPD】更新后端服务字段
    更新后端服务字段

--- backendsql/session/models.py
+++ backendsql/session/models.py
@@ -17,8 +17,10 @@ class Session(models.Model):
     updated_at = models.DateTimeField(blank=True, null=True)
 
 class SessionGoodsRelate(models.Model):
-    goods = models.OneToOneField(Goods, on_delete=models.PROTECT, related_name="as_good_session",null=True)
-    session = models.OneToOneField(Session, on_delete=models.PROTECT, related_name="as_session_good",null=True)
+    goods = models.ForeignKey(Goods, on_delete=models.PROTECT, related_name="as_good_session",null=True)
+    session = models.ForeignKey(Session, on_delete=models.PROTECT, related_name="as_session_good",null=True)
     status = models.IntegerField(blank=True,default=0)
     created_at = models.DateTimeField(blank=True, null=True)
     updated_at = models.DateTimeField(blank=True, null=True)
+    class Meta:
+        unique_together = ('goods', 'session',)
