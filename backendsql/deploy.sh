bacd0b2ca177c5f7d3f9903b8a5a0ca09ff88698
wurh <ro87630872@126.com>
2020-07-11 11:32:50 +0800

    【UPD】 add docker file suport
     add docker file suport

--- backendsql/deploy.sh
+++ backendsql/deploy.sh
@@ -1,2 +1,4 @@
+sudo docker rm -f littlemallfeservice
+sudo docker rmi -f littlemall/littlemallfe-service
 sudo docker build -t littlemall/littlemallfe-service .
 sudo docker run --name littlemallfeservice -p 7106:7106 -d littlemall/littlemallfe-service
